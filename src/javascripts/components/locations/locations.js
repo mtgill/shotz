87a3ea224929802ab251532f07cb711bdf47955d
Matt Gill <mtgill90@outlook.com>
2019-04-22 21:03:56 -0500

    buttons work

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -25,11 +25,12 @@ const shootTimeClass = (shootTime) => {
   return selectedClass;
 };
 
-const domStringBuilder = () => {
+
+const domStringBuilder = (values) => {
   let domString = '';
   domString += '<div class="container">';
   domString += '<div class="row">';
-  locations.forEach((location) => {
+  values.forEach((location) => {
     domString += '<div class="card col-2 location-cards">';
     domString += `<div id=${location.id}>`;
     domString += `<h3 class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</h3>`;
@@ -43,12 +44,52 @@ const domStringBuilder = () => {
   util.printToDom('locations', domString);
 };
 
+const sort = (e) => {
+  const buttonId = e.target.id;
+  let filterArray = [];
+  locations.forEach((location) => {
+    if (location.shootTime === 'After Dark' && buttonId === 'dark') {
+      filterArray.push(location);
+    } else if (location.shootTime === 'Morning' && buttonId === 'morning') {
+      filterArray.push(location);
+    } else if (location.shootTime === 'Evening' && buttonId === 'evening') {
+      filterArray.push(location);
+    } else if (location.shootTime === 'Afternoon' && buttonId === 'afternoon') {
+      filterArray.push(location);
+    } else if (buttonId === 'all') {
+      filterArray = locations;
+    }
+    domStringBuilder(filterArray);
+  });
+};
+
+// const filterButtonEvent = (e) => {
+//   const buttonId = e.target.id;
+//   const darkLocations = locations.filter(x => x.shootTime === 'After Dark');
+//   switch (buttonId) {
+//     case 'dark':
+//       domStringBuilder(darkLocations);
+//       break;
+//     default:
+//       domStringBuilder(locations);
+//   }
+// };
+
+const buttonEvents = () => {
+  document.getElementById('dark').addEventListener('click', sort);
+  document.getElementById('morning').addEventListener('click', sort);
+  document.getElementById('evening').addEventListener('click', sort);
+  document.getElementById('afternoon').addEventListener('click', sort);
+  document.getElementById('all').addEventListener('click', sort);
+};
+
 const initializeLocations = () => {
   locationsData.getLocationsData()
     .then((resp) => {
       const locationResults = resp.data.locations;
       locations = locationResults;
-      domStringBuilder();
+      domStringBuilder(locations);
+      buttonEvents();
     })
     .catch(err => console.error(err));
 };
